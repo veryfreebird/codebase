2c59e97cdf9585541a9576bfcde9536c7eeae2a8
veryfreebird <79344359@qq.com>
2016-03-13 20:01:38 +0800

    myproc file example

---
+++ module_test/proc_fs/.myproc.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/jamema/src/codebase_github/module_test/proc_fs/myproc.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/jamema/src/codebase_github/module_test/proc_fs/myproc.ko /home/jamema/src/codebase_github/module_test/proc_fs/myproc.o /home/jamema/src/codebase_github/module_test/proc_fs/myproc.mod.o
---
+++ module_test/proc_fs/.tmp_versions/myproc.mod
@@ -0,0 +1,2 @@
+/home/jamema/src/codebase_github/module_test/proc_fs/myproc.ko
+/home/jamema/src/codebase_github/module_test/proc_fs/myproc.o
