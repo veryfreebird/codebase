e510acdf3198812d3682f57dc7550799f2e27409
veryfreebird <79344359@qq.com>
2016-12-24 01:29:56 +0800

    add gdb remote debug

--- perf/test.c
+++ perf/test.c
@@ -1,9 +1,14 @@
+#include <stdio.h>
 //test.c 
 void foo1();
 void foo2();
 int
 main (void)
 {
-  foo1 ();
-  foo2 ();
+    int i;
+    for(i=0;;i++)
+    {
+        foo1 ();
+        foo2 ();
+    }
 }
