53e3690f3836241b54489ec1e77552ada972fa35
jamema <root@euca-10-254-137-47.eucalyptus.internal>
2017-05-22 12:48:21 +0300

    add client id

--- zeromq_tests/helloworld/hwclient.c
+++ zeromq_tests/helloworld/hwclient.c
@@ -4,22 +4,27 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main (void)
+int main (int argc, char** argv)
 {
-printf ("Connecting to hello world server…\n");
-void *context = zmq_ctx_new ();
-void *requester = zmq_socket (context, ZMQ_REQ);
-zmq_connect (requester, "tcp://localhost:5555");
-
-int request_nbr;
-for (request_nbr = 0; request_nbr != 10; request_nbr++) {
-char buffer [10];
-printf ("Sending Hello %d…\n", request_nbr);
-zmq_send (requester, "Hello", 5, 0);
-zmq_recv (requester, buffer, 10, 0);
-printf ("Received World %d\n", request_nbr);
-}
-zmq_close (requester);
-zmq_ctx_destroy (context);
-return 0;
+    printf ("Connecting to hello world server…\n");
+    void *context = zmq_ctx_new ();
+    void *requester = zmq_socket (context, ZMQ_REQ);
+    zmq_connect (requester, "tcp://localhost:5555");
+    
+    int request_nbr;
+    char buffer[10];
+    if(argc == 2)
+        sprintf(buffer, "hello%s",argv[1]);
+    else
+        sprintf(buffer, "hello");
+    for (request_nbr = 0; request_nbr != 10000; request_nbr++) {
+        //char buffer [10];
+        printf ("Sending %s %d…\n", buffer, request_nbr);
+        zmq_send (requester, buffer, strlen(buffer)+1, 0);
+        zmq_recv (requester, buffer, 10, 0);
+        printf ("Received %s %d\n", buffer, request_nbr);
+    }
+    zmq_close (requester);
+    zmq_ctx_destroy (context);
+    return 0;
 }
