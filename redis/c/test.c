433f53a58691fdc360fba2ae405eac58826aa6e5
veryfreebird <79344359@qq.com>
2017-06-23 01:52:54 +0800

    add pipeline

--- redis/c/test.c
+++ redis/c/test.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <hiredis/hiredis.h>
-
+#define max_num 1000
 int main()
 {
     redisContext *conn = redisConnect("127.0.0.1", 6379); //redis server默认端口
@@ -9,13 +9,24 @@ int main()
         printf("connection error: %d", conn->err);
     }   
 
-    for(int i = 0; i<1000; i++)
+    for(int i = 0; i<max_num; i++)
     {
-        sprintf(cmd, "set key%d %d", i);
+        sprintf(cmd, "set key%d %d", i, i);
         reply = redisCommand(conn, cmd);
         freeReplyObject(reply);
     }
 
+    for(int j=0; j<max_num; j++) //pipeline
+    {
+        sprintf(cmd, "get key%d", j);
+        redisAppendCommand(conn, cmd);
+    }
 
+    for(int k=0; k<max_num; k++)
+    {
+        redisGetReply(conn, (void **)&reply);
+        printf("Get key%d: %s\n", k, reply->str);
+        freeReplyObject(reply);
+    }
     redisFree(conn);
 }
