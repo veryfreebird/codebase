1500d447721a6343f687391eaa505ad705a87da9
James Ma <veryfreebird@gmail.com>
2019-05-21 11:10:33 +0800

    update

--- fft/fft_example.cpp
+++ fft/fft_example.cpp
@@ -1,33 +1,6 @@
 /*
-*做FFT 
-*len必须是2^k形式 
-*on == 1时是DFT，on == -1时是IDFT 
+http link: https://zhuanlan.zhihu.com/p/40505277
 */
-void fft(Complex y[],int len,int on){
-	change(y,len);
-	for(int h = 2;h <= len;h<<=1){//模拟合并过程 
-		Complex wn(cos(-on*2*PI/h),sin(-on*2*PI/h));//计算当前单位复根
-		for(int j = 0;j < len;j += h){
-			Complex w(1,0);//计算当前单位复根
-			for(int k = j;k < j + h/2;k++){
-				Complex u = y[k];
-				Complex t = w*y[k + h/2];
-				y[k] = u + t;//这就是吧两部分分治的结果加起来 
-				y[k + h/2] = u - t;
-				//后半个“step”中的ω一定和“前半个”中的成相反数
-                //“红圈”上的点转一整圈“转回来”，转半圈正好转成相反数
-                //一个数相反数的平方与这个数自身的平方相等
-				w = w*wn;
-			}
-		}
-	}
-	if(on == -1){
-		for(int i = 0;i < len;i++){
-			y[i].x /= len;
-		}
-	}
-} 
-好了现在附上全部代码，序言说过代码来自kuangbin的模板~~~~~来大家和我一起Orz一发
 
 #include<iostream>
 #include<cstdio>
