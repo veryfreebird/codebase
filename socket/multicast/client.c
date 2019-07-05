7044144559bd21ad8f2f63b6af967712120ddfd7
Ma <james.ma@nokia-sbell.com>
2019-07-05 17:46:57 +0800

    multicast works.

--- socket/multicast/client.c
+++ socket/multicast/client.c
@@ -1,60 +1,92 @@
-#include  <sys/types.h>
-#include  <sys/socket.h>
-#include  <netinet/in.h>
-#include  <string.h>
-#include  <stdio.h>
-#include  <arpa/inet.h>
-#define MAXBUF 256
-#define PUERTO 4321 
-#define GROUP "226.1.1.1"
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-int main(void) {
-    int s, n, r;
-    struct sockaddr_in srv, cli;
-    struct ip_mreq mreq;
-    char buf[MAXBUF];
+struct sockaddr_in localSock;
+struct ip_mreq group;
+struct sockaddr serveraddr;
+int serveraddr_len=200;
+int sd;
+int datalen;
+char databuf[1500];
 
-    bzero(&srv, sizeof(srv));
+int main(int argc, char *argv[])
+{
+    int loop = 1, cnt, r;
+    int err = -1;
+    sd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sd < 0)
+    {
+        perror("Opening datagram socket error");
+        exit(1);
+    }
+    else
+        printf("Opening datagram socket....OK.\n");
 
-    srv.sin_family = AF_INET;
-    srv.sin_port = htons(PUERTO);
-    if(inet_aton("226.1.1.1", &srv.sin_addr) < 0) {
-        perror("inet_aton");
-        return 1;
+    {
+        int reuse = 1;
+        if (setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, (char *)&reuse, sizeof(reuse)) < 0)
+        {
+            perror("Setting SO_REUSEADDR error");
+            close(sd);
+            exit(1);
+        }
+        else
+            printf("Setting SO_REUSEADDR...OK.\n");
     }
 
-    if((s = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        perror("socket");
-        return 1;
+    memset((char *)&localSock, 0, sizeof(localSock));
+    localSock.sin_family = AF_INET;
+    localSock.sin_port = htons(4321);
+    localSock.sin_addr.s_addr = htonl(INADDR_ANY);
+    if (bind(sd, (struct sockaddr *)&localSock, sizeof(localSock)))
+    {
+        perror("Binding datagram socket error");
+        close(sd);
+        exit(1);
     }
-   if(bind(s, (struct sockaddr *)&srv, sizeof(srv)) < 0) {
-        perror("bind");
-        return 1;
+    else
+        printf("Binding datagram socket...OK.\n");
+
+    /*设置回环许可*/
+    err = setsockopt(sd, IPPROTO_IP, IP_MULTICAST_LOOP, &loop, sizeof(loop));
+    if (err < 0)
+    {
+        perror("setsockopt():IP_MULTICAST_LOOP");
+        return -3;
     }
 
-    if(inet_aton("226.1.1.1", &mreq.imr_multiaddr) < 0) {
-        perror("inet_aton");
-        return 1;
+    group.imr_multiaddr.s_addr = inet_addr("226.1.1.1");
+    group.imr_interface.s_addr = inet_addr("10.0.2.15");
+    if (setsockopt(sd, IPPROTO_IP, IP_ADD_MEMBERSHIP, (char *)&group, sizeof(group)) < 0)
+    {
+        perror("Adding multicast group error");
+        close(sd);
+        exit(1);
     }
-     if(inet_aton("192.168.100.1", &mreq.imr_interface) < 0) {
-        perror("inet_aton");
-        return 1;
-    }
-///   mreq.imr_interface.s_addr = htonl(INADDR_ANY);
-    if (setsockopt(s,IPPROTO_IP,IP_ADD_MEMBERSHIP,&mreq,sizeof(mreq))
-            < 0) {
-        perror("setsockopt");
-        return 1;
-    }
-    n = sizeof(cli);
-    while (1) {
-        if ((r = recvfrom(s, buf, MAXBUF, 0, (struct sockaddr *)
-                        &cli, &n)) < 0) {
-            perror("recvfrom");
-        } else {
-            buf[n] = 0;
-            fprintf(stdout, "Mensaje desde %s: %s",
-                    inet_ntoa(cli.sin_addr), buf);
+    else
+        printf("Adding multicast group...OK.\n");
+
+    for (cnt = 0; cnt < 10; cnt++)
+    {
+        datalen = sizeof(databuf);
+        if ((r = recvfrom(sd, databuf, datalen, 0, &serveraddr, &serveraddr_len)) < 0)
+        {
+            perror("Reading datagram message error");
+            close(sd);
+            exit(1);
+        }
+        else
+        {
+            printf("Reading datagram message...OK.\n");
+            printf("The message from multicast server is: %s\n", databuf);
         }
     }
+    close(sd);
+    return 0;
 }
--- socket/multicast/server.c
+++ socket/multicast/server.c
@@ -5,43 +5,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
+
 
 struct in_addr localInterface;
 struct sockaddr_in groupSock;
 int sd;
 char databuf[1024] = "Multicast test message lol!";
+char databuf1[1024];
 int datalen = sizeof(databuf);
 
-
-int main (int argc, char *argv[ ])
+int main(int argc, char *argv[])
 {
+    int cnt=0;
     sd = socket(AF_INET, SOCK_DGRAM, 0);
-    if(sd < 0) {
+    if (sd < 0)
+    {
         perror("Opening datagram socket error");
         exit(1);
-    } else
+    }
+    else
         printf("Opening the datagram socket...OK.\n");
 
-
-    memset((char *) &groupSock, 0, sizeof(groupSock));
+    memset((char *)&groupSock, 0, sizeof(groupSock));
     groupSock.sin_family = AF_INET;
     groupSock.sin_addr.s_addr = inet_addr("226.1.1.1");
     groupSock.sin_port = htons(4321);
 
-
-    localInterface.s_addr = inet_addr("192.168.9.108");
-    if(setsockopt(sd, IPPROTO_IP, IP_MULTICAST_IF, (char *)&localInterface, sizeof(localInterface)) < 0) {
+    localInterface.s_addr = inet_addr("10.0.2.15");
+    if (setsockopt(sd, IPPROTO_IP, IP_MULTICAST_IF, (char *)&localInterface, sizeof(localInterface)) < 0)
+    {
         perror("Setting local interface error");
         exit(1);
-    } else
+    }
+    else
         printf("Setting the local interface...OK\n");
 
-while(1){
-    if(sendto(sd, databuf, datalen, 0, (struct sockaddr*)&groupSock, sizeof(groupSock)) < 0) {
-        perror("Sending datagram message error");}
-    else
-        printf("Sending datagram message...OK\n");}
-
-
+    while (1)
+    {
+        sprintf(databuf1, "%s %d", databuf, cnt++);
+        if (sendto(sd, databuf1, datalen, 0, (struct sockaddr *)&groupSock, sizeof(groupSock)) < 0)
+        {
+            perror("Sending datagram message error");
+        }
+        else
+            printf("Sending datagram message %d...OK\n", cnt);
+        sleep(2);
+    }
+    close(sd);
     return 0;
 }
