7a4d272dbc98b5dcc8cf1ae16dbc3d0fd51bf2f2
Ma <james.ma@nokia-sbell.com>
2019-09-10 16:32:11 +0800

    change folder name

--- qos/token_bucket/TestRateLimiter.java
+++ qos/token_bucket/TestRateLimiter.java
@@ -12,7 +12,7 @@ public class TestRateLimiter {
     public static void main(String[] args) {
  
         //新建一个每秒限制3个的令牌桶
-        RateLimiter rateLimiter = RateLimiter.create(5);
+        RateLimiter rateLimiter = RateLimiter.create(4);
  
         ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(100);
         for (int i = 0; i < 100; i++) {
@@ -20,7 +20,7 @@ public class TestRateLimiter {
                 @Override
                 public void run() {
                     //获取令牌桶中一个令牌，最多等待10秒
-                    if (rateLimiter.tryAcquire(1, 10, TimeUnit.SECONDS)) {
+                    if (rateLimiter.tryAcquire(10, 10, TimeUnit.SECONDS)) {
                         System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
                     }
                 }
