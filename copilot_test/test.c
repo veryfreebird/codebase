aed4af558e0e5c952685879622a341e60b78930d
James Ma <veryfreebird@gmail.com>
2024-06-22 10:26:17 +0800

    update

--- copilot_test/test.c
+++ copilot_test/test.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * 程序的主入口点。
+ * 
+ * 主函数是程序的开始和结束点。在这个简单的例子中，它只执行了一个动作：
+ * 打印一条欢迎信息到标准输出。
+ * 
+ * @return int 程序正常退出时返回0。
+ */
 int main() {
     printf("Hello, World!\n");
     return 0;
