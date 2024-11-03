5b11517ccf27fb9199347fc39fcfb5fe2cfb73e3
James Ma <veryfreebird@gmail.com>
2024-11-03 21:43:51 +0800

    update

--- signal_processing/rms_test.m
+++ signal_processing/rms_test.m
@@ -5,29 +5,55 @@ rng(1);
 N = 1000; % 采样点数
 fs = 1000; % 假设采样频率为1000Hz
 t = (0:N-1)/fs; % 时间向量
-noise1 = 1 + randn(1, N); % 第一路噪声
-noise2 = 2 + randn(1, N); % 第二路噪声
+
+cte1 = 1
+cte2 = 2
+
+
+dte1 = randn(1, N); % 第一路噪声
+dte2 = randn(1, N); % 第二路噪声
+
+te1 = cte1 + dte1
+te2 = cte2 + dte2
 
 % 计算每个时间点的瞬时RMS值
-instant_rms = sqrt((noise1.^2 + noise2.^2));
+instant_rms = sqrt((te1.^2 + te2.^2));
+
+instant_rms1 = cte1 + cte2 + sqrt((dte1.^2 + dte2.^2));
 
 % 绘制原始噪声信号
 figure;
-subplot(3, 1, 1);
-plot(t, noise1);
+subplot(5, 1, 1);
+plot(t, te1);
 title('第一路噪声');
 xlabel('时间 (s)');
 ylabel('幅值');
 
-subplot(3, 1, 2);
-plot(t, noise2);
+subplot(5, 1, 2);
+plot(t, te2);
 title('第二路噪声');
 xlabel('时间 (s)');
 ylabel('幅值');
 
 % 绘制瞬时RMS值
-subplot(3, 1, 3);
+subplot(5, 1, 3);
+plot(t, te1 + te2, 'r', 'LineWidth', 2);
+title('两路噪声的瞬时RMS值');
+xlabel('时间 (s)');
+ylabel('瞬时RMS值');
+legend('瞬时RMS值');
+
+% 绘制瞬时RMS值
+subplot(5, 1, 4);
 plot(t, instant_rms, 'r', 'LineWidth', 2);
+title('两路噪声的瞬时RMS值');
+xlabel('时间 (s)');
+ylabel('瞬时RMS值');
+legend('瞬时RMS值');
+
+% 绘制瞬时RMS值
+subplot(5, 1, 5);
+plot(t, instant_rms1, 'r', 'LineWidth', 2);
 title('两路噪声的瞬时RMS值');
 xlabel('时间 (s)');
 ylabel('瞬时RMS值');
